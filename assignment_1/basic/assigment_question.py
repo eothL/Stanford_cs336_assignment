d6bf0e6b48f1ffa5dcce01ff5fa64d43ba2e52fd
Theo Lir <theoxlin@gmail.com>
2026-02-01 21:14:07 +0800

    adding saving file and polishing function

--- assignment_1/basic/assigment_question.py
+++ assignment_1/basic/assigment_question.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from .train_bpe import train_bpe_heap
 from .pretokenization import count_pretokens_parallel
+from .bytes_utils import bytes_to_unicode_text
 
 import json 
 from pathlib import Path
@@ -77,13 +78,21 @@ def run_train_bpe(
     
     return vocab, merges
 
-def save_vocab_merges(vocab, merges, vocab_path, merges_vocab):
 
-    return
+def save_vocab_merges(vocab, merges, vocab_path, merges_path):
+    """return a json file of the vocab with mapping {token_str: token_id}"""
 
-def train_bpe_tinystories(data_folder_path, vocab_size, special_tokens):
-    """return answer from a and b for tinystories"""
-    dataset = "tinystories_train.txt"
+    vocab_json = {bytes_to_unicode_text(token_bytes):token_id for token_id, token_bytes in sorted(vocab.items())}
+    with open(vocab_path, "w", encoding="utf-8") as f:
+        json.dump(vocab_json, f, ensure_ascii=False) # not escaping non-ASCII characters
+        
+    with open(merges_path, "w", encoding="utf-8") as f:
+        for a, b in merges:
+            f.write(f"{bytes_to_unicode_text(a)} {bytes_to_unicode_text(b)}\n")
+        
+
+def train_bpe(data_folder_path, vocab_size, special_tokens, dataset):
+    """return vocab and merges for a specific dataset"""
     data_path = os.path.join(data_folder_path, dataset)
 
     print("*"*25,f"Start training tokenizer on {dataset} datset", "*"*25)   
@@ -91,18 +100,17 @@ def train_bpe_tinystories(data_folder_path, vocab_size, special_tokens):
     pr_ts.enable()
     vocab, merges = run_train_bpe(data_path,vocab_size,special_tokens)
     pr_ts.disable()
-    # memory took
 
     # longest token
     print("longest token in vocab:", max(vocab.values,key= len))
 
 
-    print("*"*25,"result on Tinystories datset", "*"*25)
+    print("*"*25,f"result on {dataset} datset", "*"*25)
     result_ts = pstats.Stats(pr_ts)
     result_ts.sort_stats(pstats.SortKey.TIME)
     result_ts.print_stats(10)
 
-    print("-"*25,"Analysis of the profile", "-"*25)
+    print("-"*25,"Profile Analysis", "-"*25)
     print("""Updating the heap and pair_counts is taking most of the time in the tokenizer process""")
     return vocab, merges
 
@@ -110,14 +118,24 @@ def train_bpe_tinystories(data_folder_path, vocab_size, special_tokens):
 def main():
     special_tokens= ["<|endoftext|>"]    
     saving_path = "artifacts"
-    HERE = os.path.dirname(os.path.abspath(__file__))
+    HERE = os.path.dirname(os.path.abspath(__file__)) # .../Stanford/assignment_1/basic
     DATA_FOLDER = "data"
+    ARTIFACTS_FOLDER = "artifacts"
     data_folder_path = os.path.join(HERE, "..", DATA_FOLDER)
+    artifact_folder_path = os.path.join(HERE, "..", ARTIFACTS_FOLDER)
+    vocab_path_ts = os.path.join(artifact_folder_path,"vocab_10k.json")
+    merge_path_ts =  os.path.join(artifact_folder_path,"merges_10k.txt")
+    vocab_path_owt = os.path.join(artifact_folder_path,"vocab_32k.json")
+    merge_path_owt =  os.path.join(artifact_folder_path,"merges_32k.txt")
+    dataset = ["tinystories_train.txt","openwebtext_train.txt"]
+    
     print("="*25,"Start training tokenizer", "="*25)   
-
-    train_bpe_tinystories(data_folder_path,vocab_size=10000,special_tokens=special_tokens)
-    train_bpe_owt(data_folder_path,vocab_size=32000,special_tokens=special_tokens)
-    return 
+    vocab_ts, merge_ts   =  train_bpe(data_folder_path,vocab_size=10000,special_tokens=special_tokens,dataset=dataset[0])
+    vocab_owt, merge_owt =  train_bpe(data_folder_path,vocab_size=32000,special_tokens=special_tokens,dataset=dataset[1])
+    
+    save_vocab_merges(vocab_ts,merge_ts,vocab_path_ts,merge_path_ts)
+    save_vocab_merges(vocab_owt,merge_owt,vocab_path_owt,merge_path_owt)
 
 if __name__== "__main__":
     main()
+
