f6e16ea995d22a4683665918bbcdd80947e92bac
Theo Lir <theoxlin@gmail.com>
2026-02-12 15:48:56 +0800

    passing AdamW test

--- assignment_1/basic/model.py
+++ assignment_1/basic/model.py
@@ -417,8 +417,46 @@ class SGD(torch.optim.Optimizer):
         return loss
     
 
-
 class AdamW(torch.optim.Optimizer):
-    def __init__(self, params, defaults):
+    def __init__(self, params, lr=1e-3, betas = (0.9, 0.999), eps = 1e-8, weight_decay = 0.0):
+        defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay} 
         super().__init__(params, defaults)
-        
+
+    @torch.no_grad()   
+    def step(self, closure: Optional[Callable]= None):
+        loss = None 
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            lr = group["lr"]
+            beta1, beta2 = group["betas"]
+            eps = group["eps"]
+            wd = group["weight_decay"]
+
+            for p in group["params"]:
+                grad = p.grad
+                if p.grad is None:
+                    continue
+
+                state = self.state[p]
+                if len(state) == 0:
+                    state["step"] = 0
+                    state["m"] = torch.zeros_like(p.data)
+                    state["v"] = torch.zeros_like(p.data)
+                
+                state["step"] += 1
+                t = state["step"]
+                m, v = state["m"], state["v"]
+
+                m.mul_(beta1).add_(grad, alpha = 1 - beta1) # equivalent to m = beta1 * m + (1 - beta1) * grad
+                v.mul_(beta2).addcmul_(grad, grad, value = 1 - beta2)# v = beta2 * v + (1 - beta2) * grad * grad
+                
+                step_size = lr * (math.sqrt(1 - beta2**t) / (1 - (beta1)**t))
+                p.data.mul_(1 - lr * wd)
+                p.data.addcdiv_(m, v.sqrt().add_(eps), value = -step_size)
+                state["m"] = m
+                state["v"] = v
+                state["step"] = t
+        return loss
