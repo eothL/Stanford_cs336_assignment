8259073e3635dbd52651b7b23c0d55ae31b52d63
Theo Lir <theoxlin@gmail.com>
2026-02-13 15:12:12 +0800

    change data_loading to vectorize solution instead of looping

--- assignment_1/basic/train.py
+++ assignment_1/basic/train.py
@@ -3,13 +3,10 @@ import numpy.typing as npt
 
 def data_loading(x: npt.NDArray, batch_size: int, context_length: int, device: str = "cpu") -> tuple[torch.Tensor, torch.Tensor]:
     x_t = torch.as_tensor(x)
-    input_sequences = torch.empty((batch_size, context_length), device = device, dtype= torch.long)
-    next_token_targets = torch.empty((batch_size, context_length), device = device, dtype= torch.long)
 
-    starters = torch.randint(0, len(x)- context_length, (batch_size,)) # tensor of size batch with value between 0 and len(x) - context_length
-    for i in range(batch_size):
-        start = starters[i]
-        input_sequences[i] = x_t[start: start + context_length]
-        next_token_targets[i] = x_t[start + 1: start + context_length + 1]
+    starts = torch.randint(0, len(x_t)- context_length, (batch_size,), device= device) # tensor of size batch with value between 0 and len(x) - context_length
+    offsets = torch.arange(context_length, device= device)
+
+    idx = starts[:, None] + offsets[None,:]
         
-    return (input_sequences, next_token_targets)
+    return (x_t[idx], x_t[idx+1])
