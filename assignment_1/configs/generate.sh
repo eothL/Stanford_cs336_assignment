18c203344649536694cbdd0b4410a90ad42ca859
Theo Lir <theoxlin@gmail.com>
2026-02-16 21:39:04 +0800

    generating a small story

--- assignment_1/configs/generate.sh
+++ assignment_1/configs/generate.sh
@@ -1,6 +1,8 @@
 python -m basic.generate \
   --checkpoint basic/artifacts/model/result_tinystories_example_ts_1_4874.pth \
-  --vocab-file basic/artifacts/vocab_10k.json \ 
-  --merge-file basic/artifacts/merge_10k.tkt \
-  --config configs/train_ts.yaml \
-  --prompt "Once upon a time"
+  --vocab-file basic/artifacts/vocab_10k.json \
+  --merge-file basic/artifacts/merges_10k.txt \
+  --config configs/train_ts_exp.yaml \
+  --prompt "Once upon a time" \
+  --seed 93 \
+  --max-tokens 10000 
