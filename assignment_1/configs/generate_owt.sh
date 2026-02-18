e18a7350d483ee052a42ffa11ac5f9aab484966f
Theo Lir <theoxlin@gmail.com>
2026-02-18 15:02:01 +0800

    update to medium config

--- assignment_1/configs/generate_owt.sh
+++ assignment_1/configs/generate_owt.sh
@@ -1,9 +1,8 @@
 python -m basic.generate \
-  --checkpoint basic/artifacts/model/result_L36-H20-D1280-ctx512-bs3-lr0.001-wd0.01-3b53f084_owt_1_5000.pth\
+  --checkpoint basic/artifacts/model/result_L24-H16-D1024-ctx512-bs24-lr0.001-wd0.01-feb384eb_owt_1_3146.pth \
   --vocab-file basic/artifacts/vocab_32k.json \
   --merge-file basic/artifacts/merges_32k.txt \
-  --config configs/train_owt.yaml \
+  --config configs/train_owt_medium.yaml \
   --prompt "Once upon a time" \
-  --batch-size 3 \
   --seed 93 \
   --max-tokens 10000 
