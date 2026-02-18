Correcting qk_norm implementation and  memory issue with loss accumulating as a tensor in the computation graph

assignment_1/basic/model.py
@@ -240,7 +240,13 @@ class scaled_dot_product_attention(nn.Module):
         super().__init__()
         self.mask = mask.to(device=device) if mask is not None else None
         
-    def forward(self, Q:Float[Tensor, "... seq_len d_k"], K:Float[Tensor, "... seq_len d_k"], V: Float[Tensor, "... seq_len d_v"], tau:Float,) -> Float[Tensor, "... seq_len d_v"]:
+    def forward(
+        self,
+        Q: Float[Tensor, "... seq_len d_k"],
+        K: Float[Tensor, "... seq_len d_k"],
+        V: Float[Tensor, "... seq_len d_v"],
+        tau: Float[Tensor, ""] | None = None,
+    ) -> Float[Tensor, "... seq_len d_v"]:
         d_k = Q.shape[-1]
         softmax = Softmax(dim=-1)
         if tau is not None:

assignment_1/basic/train.py
@@ -112,7 +112,8 @@ class TransformerLM(nn.Module):
                 bias= bias,
                 remove_rope= remove_rope,
                 remove_rmsnorm= remove_rmsnorm,
-                use_post_norm=use_post_norm
+                use_post_norm=use_post_norm,
+                use_qk_norm=use_qk_norm,
                 ) for _ in range(num_layers)])
         
         self.lm_head = model.Linear(in_features=d_model, out_features=vocab_size, device= device, bias=bias)
@@ -149,7 +150,7 @@ def run_epoch(
         LM.eval()
         context = torch.no_grad()
     
-    total_loss = 0
+    total_loss = 0.0
     total_sample = 0
     with context:
         x, y = loader()
@@ -168,10 +169,11 @@ def run_epoch(
             optimizer.step()
 
         batch_size = logits.size(0)
-        total_loss += loss * batch_size
+        # if we use loss instead of loss.detach().item(), we will accumulate the tensors in the computation graph
+        total_loss += loss.detach().item() * batch_size 
         total_sample += batch_size
 
-    avg_loss = total_loss/total_sample
+    avg_loss = total_loss / total_sample
     return avg_loss
 
 